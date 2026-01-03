2712e74b1dea4d2e10ac38828423911cd6467f2e
Martin Martinsson <martinsson.martin03@gmail.com>
2026-01-03 12:05:45 +0100

    Fixed flex for different cards, tested with multiple desktops

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -4,16 +4,18 @@ export default function Card({ id, name, onClickWake, onClickShutdown }) {
   return (
     <div className="border p-4 rounded-lg shadow-md m-4 bg-white">
       <h2 className="text-xl font-bold mb-2">{name}</h2>
-      <button
-        className="bg-green-400 text-white px-4 py-2 rounded hover:bg-green-600"
-        onClick={() => onClickWake(id)}>
-        Wake
+      <div className="flex gap-2 whitespace-nowrap">
+        <button
+          className="flex-1 border-solid border-black border-2 bg-green-400 text-white px-4 py-2 rounded hover:bg-green-600"
+          onClick={() => onClickWake(id)}>
+          Wake
         </button>
         <button
-            className="bg-red-400 text-white px-4 py-2 rounded hover:bg-red-600"
-            onClick={() => onClickShutdown(id)}>
-            Turn off
-      </button>
+          className="flex-1 border-solid border-black border-2 bg-red-400 text-white px-4 py-2 rounded hover:bg-red-600"
+          onClick={() => onClickShutdown(id)}>
+          Turn off
+        </button>
+      </div>
     </div>
   );
 }
--- app/services/server.ts
+++ app/services/server.ts
@@ -5,6 +5,11 @@ export function getCards() {
 
 function getDesktops () {
     return [
-        {id: 1, name: "Desktop", mac: "XXXXXXXX", ip: "192.168.0.201"}
+        {id: 1, name: "Desktop", mac: "XXXXXXXX", ip: "192.168.0.201"},
+        {id: 2, name: "Desktop", mac: "XXXXXXXX", ip: "192.168.0.201"},
+        {id: 3, name: "Desktop", mac: "XXXXXXXX", ip: "192.168.0.201"},
+        {id: 4, name: "Desktop", mac: "XXXXXXXX", ip: "192.168.0.201"},
+        {id: 5, name: "Desktop", mac: "XXXXXXXX", ip: "192.168.0.201"},
+        {id: 6, name: "Desktop", mac: "XXXXXXXX", ip: "192.168.0.201"}
     ]
 }
