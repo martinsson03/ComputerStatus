c7d4d932d4014b5e3faf2ea84cb21675cc361963
Martin Martinsson <martinsson.martin03@gmail.com>
2026-01-03 15:16:47 +0100

    Fixed header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,11 +1,9 @@
 export default function Header() {
   return (
-    <header className="bg-gray-800 shadow-md">
-      <div className="max-w-7xl mx-auto px-6 py-4 flex">
-        <h1 className="text-3xl font-bold text-white tracking-wide text-left">
+      <div className="bg-gray-800 shadow-md mx-auto px-6 py-4">
+        <h1 className="text-3xl font-bold text-white tracking-wide text-center md:text-left">
           ComputerStatus
         </h1>
       </div>
-    </header>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,16 @@ export default function App() {
   const cards = getDesktops()
 
   return (
-    <div className="flex flex-wrap">
-      {cards.map((card) => (
-        <Card
-          key={card.id}
-          id={card.id}
-          name={card.name}
-          onClickWake={wakeDesktop}
-          onClickShutdown={shutdownDesktop}
-        />
-      ))}
-    </div>
-  );
+<div className="flex flex-wrap">
+    {cards.map((card) => (
+      <Card
+        key={card.id}
+        id={card.id}
+        name={card.name}
+        onClickWake={wakeDesktop}
+        onClickShutdown={shutdownDesktop}
+      />
+    ))}
+  </div>
+  )
 }
