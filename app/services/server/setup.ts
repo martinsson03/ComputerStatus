3ceb605032eb3d6712bc2682cbc6ee71081c8767
Martin Martinsson <martinsson.martin03@gmail.com>
2026-01-03 15:24:44 +0100

    Config extracted from source code

--- app/services/server/setup.ts
+++ app/services/server/setup.ts
@@ -1,3 +1,5 @@
+import desktopsConfig from '@/config.json'
+
 export type Desktop = {
   id: number;
   name: string;
@@ -5,14 +7,8 @@ export type Desktop = {
   ip: string;
 };
 
-const desktops: Desktop[] = [
-  { id: 1, name: "Desktop", mac: "XXXXXXXX", ip: "192.168.0.201" },
-  { id: 2, name: "Desktop", mac: "XXXXXXXX", ip: "192.168.0.202" },
-  { id: 3, name: "Desktop", mac: "XXXXXXXX", ip: "192.168.0.203" },
-  { id: 4, name: "Desktop", mac: "XXXXXXXX", ip: "192.168.0.204" },
-  { id: 5, name: "Desktop", mac: "XXXXXXXX", ip: "192.168.0.205" },
-  { id: 6, name: "Desktop", mac: "XXXXXXXX", ip: "192.168.0.206" }
-];
+// TypeScript ensures type safety
+const desktops: Desktop[] = desktopsConfig;
 
 export function getDesktops(): Desktop[] {
   return desktops;
